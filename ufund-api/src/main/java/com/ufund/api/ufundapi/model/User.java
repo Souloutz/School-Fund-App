934fcdb7243d99721b1ba75f5018887df9559d60
Howard Kong <howardkong826@gmail.com>
2024-02-23 00:49:20 -0500

    Remove unnecessary imports

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/User.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/User.java
@@ -1,9 +1,6 @@
 package com.ufund.api.ufundapi.model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 import com.fasterxml.jackson.annotation.JsonProperty;
 

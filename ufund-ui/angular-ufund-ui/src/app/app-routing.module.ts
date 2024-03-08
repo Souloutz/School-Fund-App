94630428617e760fb2c82132affebe8b3316a9c4
YoItsUncle <austinkunkel85@gmail.com>
2024-03-08 14:49:47 -0500

    modifying paths

--- ufund-ui/angular-ufund-ui/src/app/app-routing.module.ts
+++ ufund-ui/angular-ufund-ui/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UfundUiLoginComponent } from './ufund-ui-login/ufund-ui-login.component';
+import { UfundUiAccountComponent } from './ufund-ui-account/ufund-ui-account.component';
+import { UfundUiHomeComponent } from './ufund-ui-home/ufund-ui-home.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full'},
-    { path: 'login', redirectTo:'/login',component: UfundUiLoginComponent},
-    //{ path: 'account', component: UfundUiAccountComponent},
+    { path: '', redirectTo: 'home',pathMatch: 'full'},
+    { path: 'home', component: UfundUiHomeComponent},
+    { path: 'login',component: UfundUiLoginComponent},
+    { path: 'account', component: UfundUiAccountComponent},
 ];
 
 @NgModule({

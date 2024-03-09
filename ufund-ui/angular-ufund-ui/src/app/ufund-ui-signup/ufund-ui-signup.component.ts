a490521e542b65ff9f2b896adbc3af5fa70c113a
YoItsUncle <austinkunkel85@gmail.com>
2024-03-09 03:13:03 -0500

    added ts functionality

--- ufund-ui/angular-ufund-ui/src/app/ufund-ui-signup/ufund-ui-signup.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/ufund-ui-signup/ufund-ui-signup.component.ts
@@ -5,12 +5,11 @@ import { CurrentUserService } from '../current.user.service';
 import { Router } from '@angular/router';
 
 import { User } from '../model/user';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: 'app-ufund-ui-login',
-  templateUrl: './ufund-ui-login.component.html',
-  styleUrl: './ufund-ui-login.component.css'
+  selector: 'app-ufund-ui-signup',
+  templateUrl: './ufund-ui-signup.component.html',
+  styleUrl: './ufund-ui-signup.component.css'
 })
 export class UfundUiSignUpComponent {
 
@@ -24,29 +23,22 @@ export class UfundUiSignUpComponent {
   
   signUp(user : User) : void
   {
-    const userInput = user;
 
-      this.userService.getUserByEmail(userInput.email).subscribe(//sends request for 
-      //to get the User with the email given by the input
-        (emailResponse: User) =>
-        {
-          console.log('User data: ', emailResponse);
-         // console.log(this.currentUserService.getCurrentUser());
-          const userResponse : User = emailResponse;
-          if(userResponse) {
-            if(userInput.password === userResponse.password)//if they are the exact same
-            {
-              console.log("Success!");
-              this.currentUserService.setCurrentUser(userResponse);
-             // console.log(this.currentUserService.getCurrentUser());
-              this.router.navigate(['']);
-            }
-            else {
-              console.log("Wrong password");
-            }
-          }
+    this.userService.addUser(user).subscribe(
+      (userInput : User) =>
+      {
+        console.log('User data: ', userInput);
+        if(userInput) {
+          console.log("Success!");
+          this.currentUserService.setCurrentUser(userInput);
+          this.router.navigate(['']);
         }
-      );
+        else
+        {
+          console.log("Could not make account");
+        }
+      }
+    );
   }
 }
 

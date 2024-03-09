06564870ffc8ea776d2e0d44c5487351342b689b
YoItsUncle <austinkunkel85@gmail.com>
2024-03-09 03:11:08 -0500

    added sign up component

--- ufund-ui/angular-ufund-ui/src/app/app-routing.module.ts
+++ ufund-ui/angular-ufund-ui/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { UfundUiLoginComponent } from './ufund-ui-login/ufund-ui-login.component';
 import { UfundUiAccountComponent } from './ufund-ui-account/ufund-ui-account.component';
 import { UfundUiHomeComponent } from './ufund-ui-home/ufund-ui-home.component';
+import { UfundUiSignUpComponent } from './ufund-ui-signup/ufund-ui-signup.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home',pathMatch: 'full'},
     { path: 'home', component: UfundUiHomeComponent},
     { path: 'login',component: UfundUiLoginComponent},
     { path: 'account', component: UfundUiAccountComponent},
+    { path: 'signup', component:UfundUiSignUpComponent}
 ];
 
 @NgModule({

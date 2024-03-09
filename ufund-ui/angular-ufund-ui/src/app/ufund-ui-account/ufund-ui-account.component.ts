a90c7c2d4a92dbaf5cafa45b55ebcdc4a6a5c935
YoItsUncle <austinkunkel85@gmail.com>
2024-03-09 03:23:51 -0500

    added a log out feature

--- ufund-ui/angular-ufund-ui/src/app/ufund-ui-account/ufund-ui-account.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/ufund-ui-account/ufund-ui-account.component.ts
@@ -4,6 +4,7 @@ import { UserService } from "../user.service";
 import { CurrentUserService } from '../current.user.service';
 
 import { User } from '../model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ufund-ui-account',
@@ -14,6 +15,21 @@ export class UfundUiAccountComponent {
 
   constructor(
     private currentUserService : CurrentUserService,
-    private userService : UserService) {}
+    private userService : UserService,
+    private router : Router) {}
 
+    currUser : User = this.currentUserService.getCurrentUser();
+
+    logOut() : void {
+      try {
+        this.currentUserService.clearCurrentUser();
+        this.currUser = this.currentUserService.getCurrentUser();
+
+        console.log("Current user reset, id is now: ", this.currUser.id);
+
+        this.router.navigate(['']);
+      } catch (error) {
+        console.log("Error logging out: ", error);
+      }
+    }
 }

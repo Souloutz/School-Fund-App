ed7ac9b744fbcc1186ea88f4d74ca423cf5ebbe6
YoItsUncle <austinkunkel85@gmail.com>
2024-03-09 21:59:54 -0500

    removes the ability to create multiple accounts with one email

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
@@ -161,6 +161,10 @@ public class UserController {
         LOG.info("POST /users/" + user.getId());
 
         try {
+            if(userDAO.getUserByEmail(user.getEmail()) != null){//if there is a user with the email
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+                //returns conflict if there is the email in use.
+            }
             User newUser = userDAO.createUser(user);
 
             if (newUser != null)

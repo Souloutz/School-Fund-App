ff8fbdf510205a0b82ca4be8f9e34a092960d066
YoItsUncle <austinkunkel85@gmail.com>
2024-03-10 22:05:00 -0400

    Initial commit

---
+++ ufund-ui/angular-ufund-ui/src/app/ufund-ui-cart/ufund-ui-cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ufund-ui-cart',
+  templateUrl: './ufund-ui-cart.component.html',
+  styleUrl: './ufund-ui-cart.component.css'
+})
+export class UfundUiCartComponent {
+
+  constructor () {}
+
+}

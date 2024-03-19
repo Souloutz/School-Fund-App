9ea516bdf3b9f1b75db256b642c49777cee570a0
YoItsUncle <austinkunkel85@gmail.com>
2024-03-18 22:47:30 -0400

    checks if user is logged in first

--- ufund-ui/angular-ufund-ui/src/app/gifts/gifts.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/gifts/gifts.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Gift } from '../model/gift';
 import { GiftService } from '../gift.service';
+import { UserService } from '../user.service';
+import { CurrentUserService } from '../current.user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gifts',
@@ -11,7 +14,10 @@ import { GiftService } from '../gift.service';
 export class GiftsComponent implements OnInit {
   gifts: Gift[] = [];
 
-  constructor(private giftService: GiftService) { }
+  constructor(private giftService: GiftService,
+              private userService : UserService,
+              private currUserService : CurrentUserService,
+              private router : Router) { }
 
   ngOnInit(): void {
     this.getGifts();
@@ -22,4 +28,14 @@ export class GiftsComponent implements OnInit {
     .subscribe(gifts => this.gifts = gifts);
   }
 
+  //adds the selected gift to the cart
+  addToCart(gift : Gift) {
+    if(this.currUserService.isUserLoggedIn()) {
+      this.userService.addItemToCart(this.currUserService.getCurrentUser().id,
+                                     this.giftService.createItemFromGift(gift, 1));
+    } else {
+      this.router.navigate(['/login']);
+    }
+
+  }
 }

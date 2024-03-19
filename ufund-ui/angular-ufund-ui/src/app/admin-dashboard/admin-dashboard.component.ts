e38dc41a33b17f0f658836be70358f0bd6958ea3
Howard Kong <howardkong826@gmail.com>
2024-03-19 01:30:52 -0400

    Rename and convert admin dashboard to be standalone

---
+++ ufund-ui/angular-ufund-ui/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-admin-dashboard',
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.css'
+})
+export class AdminDashboardComponent {
+
+}

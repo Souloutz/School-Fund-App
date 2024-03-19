efa8c20ef925d993bef3c96f3cdec356757b9f05
Howard Kong <howardkong826@gmail.com>
2024-03-19 01:29:54 -0400

    Rename and convert cart component to be standalone

--- ufund-ui/angular-ufund-ui/src/app/cart/cart.component.spec.ts
+++ ufund-ui/angular-ufund-ui/src/app/cart/cart.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UfundUiCartComponent } from './ufund-ui-cart.component';
+import { CartComponent } from './cart.component';
 
-describe('UfundUiCartComponent', () => {
-  let component: UfundUiCartComponent;
-  let fixture: ComponentFixture<UfundUiCartComponent>;
+describe('CartComponent', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UfundUiCartComponent]
+      declarations: [CartComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(UfundUiCartComponent);
+    fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

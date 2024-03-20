56785ef0dcf3115c8692833305aa3910091eee7a
Howard Kong <howardkong826@gmail.com>
2024-03-20 15:01:57 -0400

    Port pushed changes to renamed admin dashboard component

--- ufund-ui/angular-ufund-ui/src/app/admin-dashboard/admin-dashboard.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-admin-dashboard',
   imports: [
+    NgFor,
     RouterLink
   ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+  objects = [
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
+    {name: "ITEM", description: "Replace these items with the acutal list once thats made"}
+  ]; 
 
+  selected(object: any): void {
+    console.log('Item clicked:', object);
+  } 
 }

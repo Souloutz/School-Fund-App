4b5075499d1bf992365160f872f8de5c522d06b4
YoItsUncle <austinkunkel85@gmail.com>
2024-03-20 18:26:10 -0400

    adding and deleting gifts working

--- ufund-ui/angular-ufund-ui/src/app/admin-dashboard/admin-dashboard.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgFor } from '@angular/common';
+import { Gift } from '../model/gift';
+import { GiftService } from '../services/gift.service';
 
 @Component({
   standalone: true,
@@ -13,22 +15,47 @@ import { NgFor } from '@angular/common';
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
-  objects = [
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"},
-    {name: "ITEM", description: "Replace these items with the acutal list once thats made"}
-  ]; 
+
+  constructor(private giftService : GiftService) {}
+
+  gifts : Gift[] = [];
+
+  ngOnInit() :void {
+    this.getGifts();
+  }
 
   selected(object: any): void {
     console.log('Item clicked:', object);
   } 
+
+  getGifts()
+  {
+    this.giftService.getGifts().subscribe((gifts : Gift[]) => this.gifts = gifts);
+  }
+
+  addGift(name : string, description : string, passedPrice : string) : void{
+    if(!name){return;}
+    if(!description){return;}
+    if(!passedPrice){return;}
+
+    const price : number = parseInt(passedPrice);
+
+    console.log("Passed info: ",name,description,price);
+
+    this.giftService.addGift({ id: 0,
+                               name, 
+                               description, 
+                               price,
+                               priority:0,
+                               amount_needed:0})
+    .subscribe(gift => {
+      this.gifts.push(gift);
+      console.log(gift);
+    });
+  }
+
+  deleteGift(gift : Gift) {
+    this.gifts = this.gifts.filter(g => g!== gift);
+    this.giftService.deleteGift(gift.id).subscribe();
+  }
 }

5b29f8bb27d51563c8106f3b0da3c4c8af06c701
Christopher <crb9146@rit.edu>
2024-03-20 20:02:39 -0400

    Send user to 404 if invalid gift detail ID given

--- ufund-ui/angular-ufund-ui/src/app/gift-detail/gift-detail.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/gift-detail/gift-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location, NgIf } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 import { Gift } from '../model/gift';
 import { GiftService } from '../services/gift.service';
@@ -22,7 +23,8 @@ export class GiftDetailComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private giftService: GiftService,
-    private location: Location
+    private location: Location,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -32,7 +34,13 @@ export class GiftDetailComponent implements OnInit {
   getGift(): void {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.giftService.getGift(id)
-      .subscribe(gift => this.gift = gift);
+      .subscribe(gift => {
+        if (gift) {
+          this.gift = gift;
+        } else {
+          this.router.navigate(['/404']);
+        }
+      })
   }
 
   /**

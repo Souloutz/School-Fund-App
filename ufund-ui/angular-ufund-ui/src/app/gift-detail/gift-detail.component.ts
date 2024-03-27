e9fa6c857d14f1da4f28506a00a7b843da6d2cd3
YoItsUncle <austinkunkel85@gmail.com>
2024-03-27 18:18:17 -0400

    cleaned up some stuffs

--- ufund-ui/angular-ufund-ui/src/app/gift-detail/gift-detail.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/gift-detail/gift-detail.component.ts
@@ -1,11 +1,15 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location, NgIf } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { Gift } from '../model/gift';
 import { GiftService } from '../services/gift.service';
 import { FormsModule } from '@angular/forms';
+
+import { CurrentUserService } from '../services/current.user.service';
+import { UserService } from '../services/user.service';
+import { User } from '../model/user';
 
 @Component({
   standalone: true,
@@ -15,19 +19,24 @@ import { FormsModule } from '@angular/forms';
     NgIf
   ],
   templateUrl: './gift-detail.component.html',
-  styleUrls: [ './gift-detail.component.css' ]
+  styleUrls: ['./gift-detail.component.css']
 })
 export class GiftDetailComponent implements OnInit {
   gift: Gift | undefined;
+
+  isUser : boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private giftService: GiftService,
     private location: Location,
-    private router: Router
+    private router: Router,
+    private currUserService : CurrentUserService,
+    private userService : UserService
   ) {}
 
   ngOnInit(): void {
+    this.isUser = this.isUserLoggedIn();
     this.getGift();
   }
 
@@ -48,5 +57,32 @@ export class GiftDetailComponent implements OnInit {
    */
   goBack(): void {
     this.location.back();
+  }
+
+  isUserLoggedIn() : boolean{
+    return this.currUserService.isUserLoggedIn() &&
+    !this.currUserService.isAdmin();
+  }
+
+  /**
+   * Add the selected gift to the cart
+   * @param gift 
+   */
+  addToCart(gift: Gift, passedAmount : string = "1"): void {
+    if(this.currUserService.isUserLoggedIn()) {
+      console.log("Gift info: ", gift);
+
+      const amount : number = parseInt(passedAmount);
+
+      this.userService.addItemToCart(this.currUserService.getCurrentUser().email,
+                                     this.giftService.createItemFromGiftId(gift.id, gift.name, amount)).subscribe(
+                                        (user: User) => {this.currUserService.setCurrentUser(user);
+                                                 console.log('Success!', user.id)});
+      
+      console.log(this.currUserService.getCurrentUser().cart.length);
+    } 
+    
+    else 
+      this.router.navigate(['/login']);
   }
 }

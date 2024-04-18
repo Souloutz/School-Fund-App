6af60645d829ecf46794e6c4199eed1c9e5d4851
YoItsUncle <austinkunkel85@gmail.com>
2024-04-17 22:19:50 -0400

    changed some styling on the search bar

--- ufund-ui/angular-ufund-ui/src/app/components/gift-search/gift-search.component.ts
+++ ufund-ui/angular-ufund-ui/src/app/components/gift-search/gift-search.component.ts
@@ -2,15 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
 
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
 
 import { Gift } from '../../model/gift';
 import { GiftService } from '../../services/gift.service';
-import { AsyncPipe, NgFor } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf, NgClass } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { CurrentUserService } from '../../services/current.user.service';
 import { UserService } from '../../services/user.service';
-import { User } from '../../model/user';
 
 @Component({
   standalone: true,
@@ -18,13 +17,16 @@ import { User } from '../../model/user';
   imports: [
     AsyncPipe,
     NgFor,
-    RouterLink
+    RouterLink,
+    NgIf,
+    NgClass
   ],
   templateUrl: './gift-search.component.html',
-  styleUrls: [ '../gifts/gifts.component.css',
-               './gift-search.component.css' ]
+  styleUrl: './gift-search.component.css'
 })
 export class GiftSearchComponent implements OnInit {
+
+  hasGifts : boolean = false;
 
   gifts$!: Observable<Gift[]>;
 
@@ -36,11 +38,6 @@ export class GiftSearchComponent implements OnInit {
               private router: Router,
               ) {}
 
-  // push a search term into the observable stream.
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
-
   ngOnInit(): void {
     this.gifts$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
@@ -50,41 +47,21 @@ export class GiftSearchComponent implements OnInit {
       distinctUntilChanged(),
 
       // switch to new search observable each time the term changes
-      switchMap((term: string) => this.giftService.searchGifts(term)),
+      switchMap((term: string) => this.giftService.searchGifts(term)
+      .pipe(tap((gifts: Gift[]) => {
+        this.hasGifts = gifts.length > 0;
+      }))),
     );
   }
 
-  /**
-   * Add the selected gift to the cart
-   * @param gift 
-   */
-  addToCart(gift: Gift, passedAmount : string): void {
-
-    passedAmount = passedAmount == "" ? "1" : passedAmount;
-
-    if(this.currUserService.isUserLoggedIn()) {
-      console.log("Gift info: ", gift);
-
-      let amount = parseInt(passedAmount);
-  
-      if(this.isAmountOver(gift, amount)) {
-        amount = gift.amount_needed;
-      }
-
-      this.userService.addItemToCart(this.currUserService.getCurrentUser().email,
-                                     this.giftService.createItemFromGiftId(gift.id, gift.name, amount))
-                                     .subscribe((user: User) => {
-                                      this.currUserService.setCurrentUser(user);
-                                      console.log('Success!', user.id)});
-      
-      console.log(this.currUserService.getCurrentUser().cart.length);
-    } 
-    
-    else 
-      this.router.navigate(['/login']);
+  // push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
-  isAmountOver(gift: Gift, amount: number) {
-    return gift.amount_needed < amount;
+  getDetail(id : number) {
+    console.log("id: ", id);
+    this.router.navigateByUrl(`/detail/${id}`);
   }
+
 }
